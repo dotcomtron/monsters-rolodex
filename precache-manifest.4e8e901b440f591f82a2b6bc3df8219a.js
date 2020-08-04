bef589157c31ab27ababece8c0a15fa42b4dfcff
dotcomtron <polancossamuel08@gmail.com>
2020-08-04 21:01:14 +0800

    Updates

---
+++ precache-manifest.4e8e901b440f591f82a2b6bc3df8219a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db35de298c384a5582b2646d65a3ecd3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8400492244202fddbdfa",
+    "url": "/monsters-rolodex/static/css/main.3a5ea9f6.chunk.css"
+  },
+  {
+    "revision": "3c9d29464f6976dfc6d0",
+    "url": "/monsters-rolodex/static/js/2.eeae8fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.eeae8fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8400492244202fddbdfa",
+    "url": "/monsters-rolodex/static/js/main.69ef19d5.chunk.js"
+  },
+  {
+    "revision": "4f11c647a688b2abbc1d",
+    "url": "/monsters-rolodex/static/js/runtime-main.b0a3fb14.js"
+  }
+]);
